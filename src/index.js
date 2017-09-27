87efaca0bed8c23e9c893ec44fdfb2e85163ee77
Nikita Mikhaliuk <nikkita1992@mail.ru>
2017-09-27 21:39:20 +0300

    Initial solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function multiply(first, second) {
-  // your solution
+    let firstArr = first.split('');
+    let secondArr = second.split('');
+
+    function sum(arr1, arr2) {
+        let result = [];
+        for (let i = 0; i < arr2.length; i++) {
+            result.push(arr1[i] + arr2[i]);
+        }
+        for (let i = arr2.length; i < arr1.length; i++) {
+            result.push(arr1[i]);
+        }
+        return result;
+    }
+    let result = firstArr.map((value) => value * secondArr[secondArr.length - 1]);
+    let tempRes = 0;
+    for (let i = secondArr.length - 2; i >= 0; i--) {
+        tempRes = firstArr.map((value) => value * secondArr[i]);
+        result.unshift(0);
+        result = sum(result, tempRes);
+    }
+    for (let i = result.length - 1; i > 0; i--) {
+        result[i - 1] += Math.floor(result[i] / 10);
+        result[i] = result[i] % 10;
+    }
+    return result.join('');
 }
